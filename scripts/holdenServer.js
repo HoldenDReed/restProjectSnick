86081d14c11ee5eecca1b71500edc406311dbf56
Holden D Reed <holdendreed@hotmail.com>
2022-08-06 11:59:54 -0500

    Added holdenJavaScript file

---
+++ scripts/holdenServer.js
@@ -0,0 +1,62 @@
+const serverHolden = [
+    {
+        id: 1,
+        name: "Holden",
+        tables: [
+            {
+                table: 1,
+                patrons: [
+                    {
+                        patronId: 1,
+                        mealOrdered: "Tacos",
+                        drinkOrdered: "Moscow Mule",
+                        dessertOrdered: "Lava Cake",
+                        totalCost: 30
+                    },
+                    {
+                        patronId: 2,
+                        mealOrdered: "Tacos",
+                        drinkOrdered: "Moscow Mule",
+                        dessertOrdered: "Lava Cake",
+                        totalCost: 30
+                    }]
+            },
+            {
+                table: 2,
+                patrons: [
+                    {
+                        patronId: 1,
+                        mealOrdered: "Tacos",
+                        drinkOrdered: "Moscow Mule",
+                        dessertOrdered: "Lava Cake",
+                        totalCost: 30
+                    },
+                    {
+                        patronId: 2,
+                        mealOrdered: "Tacos",
+                        drinkOrdered: "Moscow Mule",
+                        dessertOrdered: "Lava Cake",
+                        totalCost: 30
+                    }]
+            },
+            {
+                table: 3,
+                patrons: [
+                    {
+                        patronId: 1,
+                        mealOrdered: "Tacos",
+                        drinkOrdered: "Moscow Mule",
+                        dessertOrdered: "Lava Cake",
+                        totalCost: 30
+                    },
+                    {
+                        patronId: 2,
+                        mealOrdered: "Tacos",
+                        drinkOrdered: "Moscow Mule",
+                        dessertOrdered: "Lava Cake",
+                        totalCost: 30
+                    }]
+            }
+        ]
+    }
+]

f0ec2ec9b0a68b2cc2fea9bb341b867ee5bfee8b
stephenrudge <sprock.ryder@gmail.com>
2022-08-20 14:01:51 -0500

    class

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# restProjectSnick
-# restprojectsnick1
+# Snickerdoodlers Resturaunt Dashboard
+
+## NSS E20 Sprint #2
+
+### Created by Deanna Hollifield, Stephen Rudge, Danny Herrmann, and Holden Reed
+
+<!-- comment next data here -->
--- scripts/serverStephen.js
+++ scripts/serverStephen.js
@@ -1,156 +1,201 @@
-const servers = [
-    {
-        id: 2,
-        name: "Stephen"
-    }
-]
-
-const stephenTables = [
-    {
-        id: 1,
-        serverId: 2,
-    },
-    {
-        id: 2,
-        serverId: 2,
-    },
-    {
-        id: 3,
-        serverId: 2,
-    }
-]
-
-const patrons = [
-    {
-        id: 1,
-        name: "John",
-        tableId: 1,
-        mealOrdered: "Burger-well done",
-        drinkOrdered: "Dr. Pepper",
-        dessertOrdered: "Lava Cake",
-        totalCost: 25,
-        isOrderCompleted: true,
-    },
-    {
-        id: 2,
-        name: "Susie",
-        tableId: 1,
-        mealOrdered: "sushi",
-        drinkOrdered: "Sprite",
-        dessertOrdered: "Ice Cream",
-        totalCost: 40,
-        isOrderCompleted: false,
-    },
-    {
-        id: 3,
-        name:"jackson",
-        tableId: 1,
-        mealOrdered: "Tacos",
-        drinkOrdered: "Water",
-        dessertOrdered: "Brownie",
-        totalCost: 35,
-        isOrderCompleted: true,
-    },
-    {
-        id: 4,
-        name:"Holly",
-        tableId: 1,
-        mealOrdered: "Chicken wrap",
-        drinkOrdered: "Coke",
-        dessertOrdered: "Tiramisu",
-        totalCost: 20,
-        isOrderCompleted: true,
-    },
-    {
-        id: 5,
-        name:"Brad",
-        tableId: 2,
-        mealOrdered: "Burger",
-        drinkOrdered: "Dr. Pepper",
-        dessertOrdered: "Lava Cake",
-        totalCost: 15.50,
-        isOrderCompleted: true,
-    },
-    {
-        id: 6,
-        name: "George",
-        tableId: 2,
-        mealOrdered: "Sushi",
-        drinkOrdered: "Sprite",
-        dessertOrdered: "Ice Cream",
-        totalCost: 12.75,
-        isOorderCompleted: true,
-    },
-    {
-        id: 7,
-        name: "Riley",
-        tableId: 2,
-        mealOrdered: "Tacos",
-        drinkOrdered: "Water",
-        dessertOrdered: "Brownie",
-        totalCost: 12.75,
-        isOrderCompleted: true,
-    },
-    {
-        id: 8,
-        name: "Tina",
-        tableId: 2,
-        mealOrdered: "Wrap",
-        drinkOrdered: "Coke",
-        dessertOrdered: "Tiramisu",
-        totalCost: 22.50,
-        isOrderCompleted: true,
-    },
-    {
-        id: 9,
-        name: "Johanna",
-        tableId: 3,
-        mealOrdered: "Wrap",
-        drinkOrdered: "Coke",
-        dessertOrdered: "Gingerbread",
-        totalCost: 16,
-        isOrderCompleted: true,
-    },
-    {
-        id: 10,
-        name: "Adam",
-        tableId: 3,
-        mealOrdered: "Wrap",
-        drinkOrdered: "Coke",
-        dessertOrdered: "Mazurek",
-        totalCost: 23,
-        isOrderCompleted: true,
-    },
-    {
-        id: 11,
-        name: "Yolanda",
-        tableId: 3,
-        mealOrdered: "wrap",
-        drinkOrdered: "Coke",
-        dessertOrdered: "Ontbijtkoek",
-        totalCost: 27,
-        isOrderCompleted: true,
-    },
-    {
-        id: 12,
-        name: "Jessica",
-        tableId: 3,
-        mealOrdered: "Chicken Alfredo",
-        drinkOrdered: "7Up",
-        dessertOrdered: "Sfouf",
-        totalCost: 45,
-        isOrderCompleted: true,
-    },
 
 
-]
+// const servers = [
+//     {
+//         id: 3,
+//         name: "Stephen"
+//     }
+// ]
+
+// const stephenTables = [
+//     {
+//         id: 26,
+//         serverId: 3,
+//     },
+//     {
+//         id: 27,
+//         serverId: 3,
+//     },
+//     {
+//         id: 28,
+//         serverId: 3,
+//     },
+//     {
+//         id: 29,
+//         serverId: 3,
+//     },
+//     {
+//         id: 30,
+//         serverId: 3,
+//     },
+//     {
+//         id: 31,
+//         serverId: 3,
+//     },
+//     {
+//         id: 32,
+//         serverId: 3,
+//     },
+//     {
+//         id: 33,
+//         serverId: 3,
+//     },
+//     {
+//         id: 34,
+//         serverId: 3,
+//     },
+//     {
+//         id: 35,
+//         serverId: 3,
+//     },
+//     {
+//         id: 36,
+//         serverId: 3,
+//     },
+//     {
+//         id: 37,
+//         serverId: 3,
+//     },
+// ]
+
+// const patrons = [
+//     {
+//         id: 26,
+//         // name: "John",
+//         tableId: 1,
+//         mealOrdered: "Burger-well done",
+//         drinkOrdered: "Dr. Pepper",
+//         dessertOrdered: "Lava Cake",
+//         totalCost: 25,
+//         isOrderCompleted: true,
+//     },
+//     {
+//         id: 27,
+//         // name: "Susie",
+//         tableId: 1,
+//         mealOrdered: "sushi",
+//         drinkOrdered: "Sprite",
+//         dessertOrdered: "Ice Cream",
+//         totalCost: 40,
+//         isOrderCompleted: false,
+//     },
+//     {
+//         id: 28,
+//         // name:"jackson",
+//         tableId: 1,
+//         mealOrdered: "Tacos",
+//         drinkOrdered: "Water",
+//         dessertOrdered: "Brownie",
+//         totalCost: 35,
+//         isOrderCompleted: true,
+//     },
+//     {
+//         id: 29,
+//         // name:"Holly",
+//         tableId: 1,
+//         mealOrdered: "Chicken wrap",
+//         drinkOrdered: "Coke",
+//         dessertOrdered: "Tiramisu",
+//         totalCost: 20,
+//         isOrderCompleted: true,
+//     },
+//     {
+//         id: 30,
+//         // name:"Brad",
+//         tableId: 2,
+//         mealOrdered: "Burger",
+//         drinkOrdered: "Dr. Pepper",
+//         dessertOrdered: "Lava Cake",
+//         totalCost: 15.50,
+//         isOrderCompleted: true,
+//     },
+//     {
+//         id: 31,
+//         // name: "George",
+//         tableId: 2,
+//         mealOrdered: "Sushi",
+//         drinkOrdered: "Sprite",
+//         dessertOrdered: "Ice Cream",
+//         totalCost: 12.75,
+//         isOorderCompleted: true,
+//     },
+//     {
+//         id: 32,
+//         // name: "Riley",
+//         tableId: 2,
+//         mealOrdered: "Tacos",
+//         drinkOrdered: "Water",
+//         dessertOrdered: "Brownie",
+//         totalCost: 12.75,
+//         isOrderCompleted: true,
+//     },
+//     {
+//         id: 33,
+//         // name: "Tina",
+//         tableId: 2,
+//         mealOrdered: "Wrap",
+//         drinkOrdered: "Coke",
+//         dessertOrdered: "Tiramisu",
+//         totalCost: 22.50,
+//         isOrderCompleted: true,
+//     },
+//     {
+//         id: 34,
+//         // name: "Johanna",
+//         tableId: 3,
+//         mealOrdered: "Wrap",
+//         drinkOrdered: "Coke",
+//         dessertOrdered: "Gingerbread",
+//         totalCost: 16,
+//         isOrderCompleted: true,
+//     },
+//     {
+//         id: 35,
+//         // name: "Adam",
+//         tableId: 3,
+//         mealOrdered: "Wrap",
+//         drinkOrdered: "Coke",
+//         dessertOrdered: "Mazurek",
+//         totalCost: 23,
+//         isOrderCompleted: true,
+//     },
+//     {
+//         id: 36,
+//         // name: "Yolanda",
+//         tableId: 3,
+//         mealOrdered: "wrap",
+//         drinkOrdered: "Coke",
+//         dessertOrdered: "Ontbijtkoek",
+//         totalCost: 27,
+//         isOrderCompleted: true,
+//     },
+//     {
+//         id: 37,
+//         // name: "Jessica",
+//         tableId: 3,
+//         mealOrdered: "Chicken Alfredo",
+//         drinkOrdered: "7Up",
+//         dessertOrdered: "Sfouf",
+//         totalCost: 45,
+//         isOrderCompleted: true,
+//     },
+
+
+// ]
+
+import {getTableData} from './restdata.js'
+import {getServerData} from './restdata.js'
+import {getPatronData} from './restdata.js'
+const tables = getTableData()
+const patrons = getPatronData()
+const servers = getServerData()
 
 
 let serverContent = "";
 
-serverContent += `<h3>Server: ${servers[0].name}</h3>`;
+serverContent += `<h3>Server: ${servers[2].name}</h3>`;
 
-stephenTables.forEach((table) => {
+tables.forEach((table) => {
   const patronsAtTable = patrons.filter(
     (patron) => patron.tableId === table.id
   );

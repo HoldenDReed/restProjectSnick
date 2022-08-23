f4e5f8340a82933687defd941b2c6234c0e17fbe
stephenrudge <sprock.ryder@gmail.com>
2022-08-23 18:50:25 -0500

    adding menu

---
+++ scripts/menu.js
@@ -0,0 +1,401 @@
+const menuMeal = [
+
+    {
+        id: 1,
+        mealName: "Bacon Burger"
+    },
+    {
+        id: 2,
+        mealName:"" ,    
+    },
+    {
+        id: 3,
+        mealName: "",
+    },
+    {
+        id: 4,
+        mealName:"" ,    
+    }, 
+    {
+        id: 5,
+        mealName:"" ,    
+    },   
+    {
+        id: 6,
+        mealName:"" ,    
+    },   
+    {
+        id: 7,
+        mealName:"" ,    
+    },
+    {
+        id: 8,
+        mealName:"" ,    
+    },
+    {
+        id: 9,
+        mealName:"" ,    
+    },
+    {
+        id:9,
+        mealName: ""    
+    },
+    {
+        id: 10,
+        mealName: "",
+    },
+    {
+        id: 11,
+        mealName: "",
+    },
+    {
+        id: 12,
+        mealName: "",
+    },
+    {
+        id: 13,
+        mealName: "",
+    },
+    {
+        id:14,
+        mealName:""
+,    },
+    {
+        id: 15,
+        mealName: "",
+    },
+    {
+        id: 16,
+        mealName: "",
+    },
+    {
+        id: 17,
+        mealName: "",
+    },
+    {
+        id: 18,
+        mealName: "",
+    },
+    {
+        id: 19,
+        mealName: "",
+    },
+    {
+        id: 20,
+        mealName: "",
+    },
+    {
+        id: 21,
+        mealName: "",
+    },
+    {
+        id: 22,
+        mealName: "",
+    },
+    {
+        id: 23,
+        mealName: "",
+    },
+    {
+        id: 24,
+        mealName: "",
+    },
+    {
+        id: 25,
+        mealName: "",
+    },
+    {
+        id: 26,
+        mealName: "",
+    },
+    {
+        id: 27,
+        mealName: "",
+    },
+    {
+        id: 28,
+        mealName: "",
+    },
+    {
+        id: 20,
+        mealName: "",
+    },
+    {
+        id: 21,
+        mealName: "",
+    },
+    {
+        id: 22,
+        mealName: "",
+    },
+    {
+        id: 23,
+        mealName: "",
+    },
+    {
+        id: 24,
+        mealName: "",
+    },
+    {
+        id: 25,
+        mealName: "",
+    },
+    {
+        id: 26,
+        mealName: "",
+    },
+    {
+        id: 27,
+        mealName: "",
+    },
+    {
+        id: 28,
+        mealName: "",
+    },
+    {
+        id: 29,
+        mealName: "",
+    },
+    {
+        id: 30,
+        mealName: "",
+    },
+    {
+        id: 31,
+        mealName: "",
+    },
+    {
+        id: 32,
+        mealName: "",
+    },
+    {
+        id: 33,
+        mealName: "",
+    },
+    {
+        id: 34,
+        mealName: "",
+    },
+    {
+        id: 35,
+        mealName: "",
+    },
+    {
+        id: 36,
+        mealName: "",
+    },
+    {
+        id: 37,
+        mealName: "",
+    },
+    {
+        id: 38,
+        mealName: "",
+    },
+    {
+        id: 39,
+        mealName: "",
+    },
+    {
+        id: 40,
+        mealName: "",
+    },
+    {
+        id: 41,
+        mealName: "",
+    },
+    {
+        id: 42,
+        mealName: "",
+    },
+    {
+        id: 43,
+        mealName: "",
+    },
+    {
+        id: 44,
+        mealName: "",
+    },
+    {
+        id: 45,
+        mealName: "",
+    },
+    {
+        id: 46,
+        mealName: "",
+    }
+]
+
+const menuDrinks = [
+
+        {
+            id: 1,
+            mealName: "",
+        },
+        {
+            id: 2,
+            mealName: ""         
+        },
+        {
+            id: 3,
+        },
+        {
+            id: 4
+        },
+        {
+            id: 5
+        },
+        {
+            id: 6
+        },
+        {
+            id: 7
+        },
+        {
+            id: 8
+        },
+        {
+            id: 9
+        },
+        {
+            id:9
+        },
+        {
+            id: 10
+        },
+        {
+            id: 11
+        },
+        {
+            id: 12
+        },
+        {
+            id: 13
+        },
+        {
+            id:14
+        },
+        {
+            id: 15
+        },
+        {
+            id: 16
+        },
+        {
+            id: 17
+        },
+        {
+            id: 18
+        },
+        {
+            id: 19
+        },
+        {
+            id: 20
+        },
+        {
+            id: 21
+        },
+        {
+            id: 22
+        },
+        {
+            id: 23
+        },
+        {
+            id: 24
+        },
+        {
+            id: 25
+        },
+        {
+            id: 26
+        },
+        {
+            id: 27
+        },
+        {
+            id: 28
+        },
+        {
+            id: 20
+        },
+        {
+            id: 21
+        },
+        {
+            id: 22
+        },
+        {
+            id: 23
+        },
+        {
+            id: 24
+        },
+        {
+            id: 25
+        },
+        {
+            id: 26
+        },
+        {
+            id: 27
+        },
+        {
+            id: 28
+        },
+        {
+            id: 29
+        },
+        {
+            id: 30
+        },
+        {
+            id: 31
+        },
+        {
+            id: 32
+        },
+        {
+            id: 33
+        },
+        {
+            id: 34
+        },
+        {
+            id: 35
+        },
+        {
+            id: 36
+        },
+        {
+            id: 37
+        },
+        {
+            id: 38
+        },
+        {
+            id: 39
+        },
+        {
+            id: 40
+        },
+        {
+            id: 41
+        },
+        {
+            id: 42
+        },
+        {
+            id: 43
+        },
+        {
+            id: 44
+        },
+        {
+            id: 45
+        },
+        {
+            id: 46
+        },
+    ]

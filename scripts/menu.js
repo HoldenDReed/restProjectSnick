b3343948894ff52d948b8a3973f15393f4ff8bcb
Danny Herrmann <dannyherrmann91@gmail.com>
2022-08-23 20:47:38 -0500

    adding desserts to menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -399,3 +399,192 @@ const menuDrinks = [
             id: 46
         },
     ]
+
+const menuDesserts = [
+    {
+        id: 1,
+        dessertName: "Lava Cake",
+        dessertDescription: "A tasty molten chocolate cake!!",
+        dessertCost: 5
+    },
+    {
+        id: 2,
+        dessertName: "Mega Cookie",
+        dessertDescription: "This cookie is so enormous it's going to blow your mind.",
+        dessertCost: 50
+    },
+    {
+        id: 3,
+        dessertName: "Ice Cream",
+        dessertDescription: "The flavor is a surprise",
+        dessertCost: 3
+    },
+    {
+        id: 4,
+        dessertName: "Vanilla Ice Cream",
+        dessertDescription: "The ever classic, ever basic, pile of vanilla ice cream.",
+        dessertCost: 3  
+    },
+    {
+        id: 5,
+        dessertName: "Strawberry Cheesecake",
+        dessertDescription: "A classic slice of cream cheese, graham cracker cumbs and a pile of strawberries.",
+        dessertCost: 5 
+    },
+    {
+        id: 6,
+        dessertName: "Chocolate Pudding",
+        dessertDescription: "A dope Hershey's cocoa pudding, come on, you want this.",
+        dessertCost: 4
+    },
+    {
+        id: 7,
+        dessertName: "Strawberry Shortcake",
+        dessertDescription: "An American classic. A sweet cake with strawberries.",
+        dessertCost: 7
+    },
+    {
+        id: 8,
+        dessertName: "Fruit Plate",
+        dessertDescription: "Fruit is dessert?",
+        dessertCost: 3
+    },
+    {
+        id: 9,
+        dessertName: "Cinnamon Donuts",
+        dessertDescription: "A tasty cinnamon donut",
+        dessertCost: 2
+    },
+    {
+        id: 10,
+        dessertName: "Chocolate Ice Cream",
+        dessertDescription: "Another classic. Ice Cream in the chocolate flavor. Get into it.",
+        dessertCost: 4
+    },
+    {
+        id: 11,
+        dessertName: "Pecan Pie",
+        dessertDescription: "Pecan pie is a pie of pecan nuts mixed with a filling of eggs, butter, and sugar.",
+        dessertCost: 6
+    },
+    {
+        id: 12,
+        dessertName: "Chocolate Chip Cookies",
+        dessertDescription: "Our chocolate chip cookies are not whatever, they're #forever.",
+        dessertCost: 5
+    },
+    {
+        id: 13,
+        dessertName: "Tiramisu",
+        dessertDescription: "Tiramisu is a coffee-flavoured Italian dessert.",
+        dessertCost: 8
+    },
+    {
+        id: 14, 
+        dessertName: "Pumpkin Pie",
+        dessertDescription: "Pumpkin pie is a dessert pie with a spiced, pumpkin-based custard filling.",
+        dessertCost: 5
+    },
+    {
+        id: 15,
+        dessertName: "Brownie",
+        dessertDescription: "A chocolate brownie or simply a brownie is a square or rectangular chocolate baked confection.",
+        dessertCost: 2
+    },
+    {
+        id: 16,
+        dessertName: "Gingerbread",
+        dessertDescription: "A moist and lightly spiced gingerbread cake with a dusting of powdered sugar and whipped cream.",
+        dessertCost: 9
+    },
+    {
+        id: 17,
+        dessertName: "Mazurek",
+        dessertDescription: "Mazurek is a variety of very sweet, flat cake baked in Poland for Easter.",
+        dessertCost: 20
+    },
+    {
+        id: 18,
+        dessertName: "Ontbijtkoek",
+        dessertDescription: "A honey cake!",
+        dessertCost: 15
+    },
+    {
+        id: 19,
+        dessertName: "Sfouf",
+        dessertDescription: "A Lebanese almond-semolina cake.",
+        dessertCost: 10
+    },
+    {
+        id: 20,
+        dessertName: "Cake",
+        dessertDescription: "Just a basic cake it's sort of a suprise ;)",
+        dessertCost: 90
+    },
+    {
+        id: 21,
+        dessertName: "Strawberry Ice Cream",
+        dessertDescription: "A Ben & Jerry's classic, straight from Vermont, come and get it.",
+        dessertCost: 5
+    },
+    {
+        id: 22,
+        dessertName: "Banana Pudding",
+        dessertDescription: "A southern classic of sweet vanilla custard, cookies, and sliced fresh bananas.",
+        dessertCost: 8
+    },
+    {
+        id: 23,
+        dessertName: "Bananas Foster",
+        dessertDescription: "Bananas and vanilla ice cream!",
+        dessertCost: 9
+    },
+    {
+        id: 24,
+        dessertName: "Fruit Sorbet Trio",
+        dessertDescription: "A tasty trio of fruit sorbet - strawberry, mango, and rasberry",
+        dessertCost: 5
+    },
+    {
+        id: 25,
+        dessertName: "Italian Wedding Cake",
+        dessertDescription: "This is like a cake for a wedding?",
+        dessertCost: 20
+    },
+    {
+        id: 26,
+        dessertName: "Double Chocolate Brownie",
+        dessertDescription: "We brownies, but also get yourself a DOUBLE chocolate brownie.",
+        dessertCost: 10
+    },
+    {
+        id: 27,
+        dessertName: "Georgia Peach Cobbler",
+        dessertDescription: "Grandma's famous peach cobbler, come and get it.",
+        dessertCost: 12
+    },
+    {
+        id: 28,
+        dessertName: "Key Lime Pie",
+        dessertDescription: "An instant classic, you know it, you love it, THE key lime pie.",
+        dessertCost: 6
+    },
+    {
+        id: 29,
+        dessertName: "Cake Pops",
+        dessertDescription: "Straight from Starbucks.",
+        dessertCost: 6
+    },
+    {
+        id: 30,
+        dessertName: "Chocolate Fondue Bar",
+        dessertDescription: "Get some fondue like it's the 70s",
+        dessertCost: 20
+    },
+    {
+        id: 31,
+        dessertName: "Blueberry Loaf",
+        dessertDescription: "An amazing loaf of blueberry bread",
+        dessertCost: 10
+    }
+]
